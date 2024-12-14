c2e5503951c961d7b95b545735e63b7ad45333e4
Dewanta Rahma Satria <dewantarahmasatria@gmail.com>
2024-12-14 22:43:35 +0700

    Add source cpp

--- KRL/KRL.cpp
+++ KRL/KRL.cpp
@@ -1,160 +1,129 @@
 #include "KRL.h"
 
-void createStasiun(string name, adrStasiun &s){
+void createStasiun(string name, adrStasiun &s) {
     s = new Stasiun;
     nama(s) = name;
-    nextStasiun(s) = NULL;
-    firstRute(s) = NULL;
-
+    nextStasiun(s) = nullptr;
+    firstRute(s) = nullptr;
 }
 
-void addStasiun(Graph &G, string nama){
-    adrStasiun newStasiun;
-    createStasiun(name, newStasiun);
-
-    if (firstStasiun(s) == NULL) {
-        firstStasiun(s) = newStasiun;
-    } else {
-        adrStasiun temp = firstStasiun(s);
-        while (nextStasiun(temp) != NULL) {
-            temp = nextStasiun(temp);
-        }
-        nextStasiun(temp) = newStasiun;
-    }
-
-}
-
-void initGraph(Graph &G){
-    firstStasiun(G) = NULL;
-
-}
-
-void createRute(string newNamaStasiun, int jarak, int harga, adrRute &r){
+void createRute(string newNamaStasiun, int jarak, int harga, adrRute &r) {
     r = new Rute;
-    namaStasiun(v) = newNamaStasiun;
-    jarak(r) = jarak;
-    harga(r) = harga;
-    nextRute(r) = NULL;
+    r->adrS = new Stasiun;
+    nama(r->adrS) = newNamaStasiun;
+    r->jarak = jarak;
+    r->harga = harga;
+    nextRute(r) = nullptr;
 }
 
-void addRute(adrRute &r, string namaStasiun, int jarak, int harga){
-    adrRute p;
-    createRute(namaStasiun, jarak, harga, p);
-    if (firstRute(r) == NULL){
-        firstRute(r) = p;
-    }else{
-        adrRute temp = firstRute(r);
-        while (nextRute(temp) != NULL){
-            temp = nextRute(temp);
-        }
-        nextRute(temp) = p;
-    }
-
+void initGraph(Graph &G) {
+    firstStasiun(G) = nullptr;
 }
 
-int degree(Graph &G, adrStasiun v){
-    int count = 0;
+void buildGraph(Graph &G) {
+    initGraph(G);
+    // Implementasi pembuatan graph sesuai kebutuhan
+}
+
+void printStasiun(Graph G) {
     adrStasiun p = firstStasiun(G);
-    while(p!=NULL){
-        adrRute temp = firstRute(p);
-        while(temp!=NULL){
-            if (nama(v)==namaStasiun(temp)){
-                count++;
-            }
-            temp = nextRute(temp);
+    while (p != nullptr) {
+        cout << "Stasiun: " << nama(p) << endl;
+        p = nextStasiun(p);
+    }
+}
+
+void printRute(Graph G) {
+    adrStasiun p = firstStasiun(G);
+    while (p != nullptr) {
+        adrRute r = firstRute(p);
+        cout << "Rute dari " << nama(p) << ":\n";
+        while (r != nullptr) {
+            cout << "  Ke: " << namaStasiun(r->adrS) << ", Jarak: " << jarak(r) << ", Harga: " << harga(r) << endl;
+            r = nextRute(r);
         }
         p = nextStasiun(p);
     }
+}
 
-    adrRute e = firstRute(v);
-    while(e!=NULL){
+void addStasiun(Graph &G, string nama) {
+    adrStasiun s;
+    createStasiun(nama, s);
+    nextStasiun(s) = firstStasiun(G);
+    firstStasiun(G) = s;
+}
+
+void addRute(adrRute &r, string namaStasiun, int jarak, int harga) {
+    adrRute newR;
+    createRute(namaStasiun, jarak, harga, newR);
+    nextRute(newR) = r;
+    r = newR;
+}
+
+void deleteStasiun(Graph &G, string namaStasiun) {
+    adrStasiun p = firstStasiun(G), q = nullptr;
+    while (p != nullptr && nama(p) != namaStasiun) {
+        q = p;
+        p = nextStasiun(p);
+    }
+    if (p != nullptr) {
+        if (q == nullptr) firstStasiun(G) = nextStasiun(p);
+        else nextStasiun(q) = nextStasiun(p);
+        delete p;
+    }
+}
+
+void deleteRute(Graph &G, string namaStasiun, string namaStasiunTujuan) {
+    adrStasiun p = firstStasiun(G);
+    while (p != nullptr && nama(p) != namaStasiun) {
+        p = nextStasiun(p);
+    }
+    if (p != nullptr) {
+        adrRute r = firstRute(p), prev = nullptr;
+        while (r != nullptr && namaStasiun(r->adrS) != namaStasiunTujuan) {
+            prev = r;
+            r = nextRute(r);
+        }
+        if (r != nullptr) {
+            if (prev == nullptr) firstRute(p) = nextRute(r);
+            else nextRute(prev) = nextRute(r);
+            delete r;
+        }
+    }
+}
+
+int degree(Graph &G, adrStasiun v) {
+    int count = 0;
+    adrRute r = firstRute(v);
+    while (r != nullptr) {
         count++;
-        e = nextRute(e);
+        r = nextRute(r);
     }
     return count;
 }
 
-adrStasiun stasiunTeramai(G Graph){
-    adrStasiun p = firstStasiun(G);
-    adrStasiun pmaks = NULL;
-    int temp, maks;
-    maks = -1;
-    while(p!=NULL){
-        temp = degree(G,p);
-        if (temp>maks){
-            pmaks = p;
-            maks = temp;
+adrStasiun stasiunTeramai(Graph G) {
+    adrStasiun p = firstStasiun(G), maxStasiun = nullptr;
+    int maxDegree = -1;
+    while (p != nullptr) {
+        int d = degree(G, p);
+        if (d > maxDegree) {
+            maxDegree = d;
+            maxStasiun = p;
         }
         p = nextStasiun(p);
     }
-    return pmaks;
+    return maxStasiun;
 }
 
-void buildGraph(Graph &G);
-void printStasiun(Graph G);
-void printRute(Graph G);
-void deleteStasiun(Graph &G, string namaStasiun);
-void deleteRute(Graph &G, string namaStasiun, string namaStasiunTujuan);
-
-void menu(){
-    int input;
-    while(input != 8){
-            cout << "---------------------------------------------------" << endl;
-            cout << "                       menu                        " << endl;
-            cout << "---------------------------------------------------" << endl;
-            cout << "1. menambahkan stasiun                             " << endl;
-            cout << "2. tampilkan semua daftar stasiun                  " << endl;
-            cout << "3. menambahkan rute                                " << endl;
-            cout << "4. tampilkan rute stasiun                          " << endl;
-            cout << "5. tampilkan rute termurah                         " << endl;
-            cout << "6. tampilkan stasiun paling ramai                  " << endl;
-            cout << "7. mencari jalur alternatif                        " << endl;
-            cout << "8. keluar                                          " << endl;
-            cout << "---------------------------------------------------" << endl;
-            cout << "input : ";
-            cin >> input;
-
-        switch(input){
-        case 1:{
-            string nama;
-            cout <<"masukan nama stasiun : " << endl;
-            cin >> nama;
-            addStasiun(G, nama);
-            cout << "data telah ditambahkan ! ";
-        }
-        case 2:{
-            printStasiun(G);
-            cout << "semua data stasiun telah di tampilkan !";
-        }
-        case 3:{
-            string nama, int jarak, int harga;
-            cout << "masukan rute : " << endl;
-            cout << "masukan jarak : " << endl;
-            cout << "masukan harga : " << endl;
-            cin >> nama;
-            cin >> jarak;
-            cin >> harga;
-        }
-        case 4:{
-            printRute(G);
-            cout << "semua data  rute telah di tampilkan !";
-        }
-        case 5:{
-
-        }
-        case 6:{
-
-        }
-        case 7:{
-
-        }
-        case 8:{
-            break;
-        }
-
-        }
-    }
+void menu() {
+    cout << "Menu Graph KRL:\n";
+    cout << "1. Tambah Stasiun\n";
+    cout << "2. Tambah Rute\n";
+    cout << "3. Hapus Stasiun\n";
+    cout << "4. Hapus Rute\n";
+    cout << "5. Cetak Stasiun\n";
+    cout << "6. Cetak Rute\n";
+    cout << "7. Stasiun Teramai\n";
+    cout << "8. Keluar\n";
 }
-
-
-
